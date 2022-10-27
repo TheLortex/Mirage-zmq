2d9e11f26bd46cad893f1fda903b1b7875ad4173
Lucas Pluvinage <lucas@tarides.com>
2022-10-27 16:45:33 +0200

    frame parser is debug

--- src/frame.ml
+++ src/frame.ml
@@ -40,7 +40,7 @@ let parser =
       Int64.to_int content_length
     else any_uint8
   in
-  Log.info (fun f -> f "Frame length: %d" content_length);
+  Log.debug (fun f -> f "Frame length: %d" content_length);
   let+ body = take content_length in
   {
     flags = Char.chr flag;
