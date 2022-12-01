2cff73ff2f3b3f63b76b4db923d884120e19f4e2
Lucas Pluvinage <lucas@tarides.com>
2022-12-01 16:12:12 +0100

    frame flags as int instead of char

--- src/frame.ml
+++ src/frame.ml
@@ -1,5 +1,5 @@
 type t = {
-  flags : char;
+  flags : int;
   (* Known issue: size is limited by max_int; may not be able to reach 2^63-1 *)
   size : int;
   body : bytes;
@@ -15,13 +15,13 @@ let make_frame body ~if_more ~if_command =
   if if_more then f := !f + 1;
   if if_command then f := !f + 4;
   if len > 255 then f := !f + 2;
-  { flags = Char.chr !f; size = len; body }
+  { flags = !f; size = len; body }
 
 let to_bytes t =
   Bytes.concat Bytes.empty
     [
-      Bytes.make 1 t.flags;
-      size_to_bytes t.size (Char.code t.flags land 2 = 2);
+      Bytes.make 1 (Char.chr t.flags);
+      size_to_bytes t.size (t.flags land 2 = 2);
       t.body;
     ]
 
@@ -35,8 +35,8 @@ let parser =
   let open Angstrom in
   let ( let* ) a f = bind a ~f in
   let ( let+ ) a f = map a ~f in
-  let* flag = any_uint8 in
-  let if_long = flag land 2 = 2 in
+  let* flags = any_uint8 in
+  let if_long = flags land 2 = 2 in
   let* content_length =
     if if_long then
       let+ content_length = BE.any_int64 in
@@ -45,18 +45,14 @@ let parser =
   in
   Log.debug (fun f -> f "Frame length: %d" content_length);
   let+ body = take content_length in
-  {
-    flags = Char.chr flag;
-    size = content_length;
-    body = Bytes.unsafe_of_string body;
-  }
+  { flags; size = content_length; body = Bytes.unsafe_of_string body }
 
-let is_more t = Char.code t.flags land 1 = 1
-let is_long t = Char.code t.flags land 2 = 2
-let is_command t = Char.code t.flags land 4 = 4
+let is_more t = t.flags land 1 = 1
+let is_long t = t.flags land 2 = 2
+let is_command t = t.flags land 4 = 4
 let get_body t = t.body
 let is_delimiter_frame t = is_more t && t.size = 0
-let delimiter_frame = { flags = Char.chr 1; size = 0; body = Bytes.empty }
+let delimiter_frame = { flags = 1; size = 0; body = Bytes.empty }
 
 let splice_message_frames list =
   List.rev_map get_body list |> List.rev |> Bytes.concat Bytes.empty
